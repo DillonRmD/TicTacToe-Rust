71a7d1d7fe205b11f2581fbc1a356fcc84484eae
DillonRmD <dillonwilliams2001@yahoo.com>
2022-08-19 20:07:01 -0500

    Cleaned up code and fully validated AI input and user input

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,93 @@
+use rand::Rng;
+use std::io::{stdin};
 
-fn mark_spot_on_game_board(mut game_board: [[u32; 3]; 3], i: &str, j: usize) -> [[u32; 3]; 3]{
+struct GameState{
+    game_over: bool,
+    game_board: [[u32; 3]; 3],
+    user_input: Vec<usize>
+}
+
+fn mark_spot_for_ai(mut game_state: GameState) -> GameState{
     
-    let mut col_input = 0;
+    let mut rng = rand::thread_rng();
 
-    if i.eq("a") {
-        col_input = 0;
+
+    let mut mark_is_valid = false;
+
+    while mark_is_valid != true{
+
+        let ai_col_input: usize = rng.gen_range(0..4);
+        let ai_row_input: usize = rng.gen_range(0..4);
+
+        let j = ai_col_input;
+        let i = ai_row_input;
+
+        if game_state.game_board[i][j] == 0 {
+            game_state.game_board[i][j] = 2;
+            mark_is_valid = true;
+        }
     }
-    else if i.eq("b"){
-        col_input = 1;
+    
+    game_state
+
+}
+
+fn mark_spot_for_user(mut game_state: GameState) -> GameState{
+    
+    let mut mark_is_valid = false;
+
+    while mark_is_valid != true{
+
+        game_state = get_user_input(game_state);
+
+        let j = game_state.user_input[0];
+        let i = game_state.user_input[1];
+
+        if game_state.game_board[i][j] != 0 {
+            println!("Space already occupied!");
+        }
+        else{
+            game_state.game_board[i][j] = 1;
+            mark_is_valid = true;
+        }
     }
-    else if i.eq("c"){
-        col_input = 2;
+    
+    game_state
+}
+
+fn get_user_input(mut game_state: GameState) -> GameState{
+
+    let mut user_input = String::new();
+    stdin().read_line(&mut user_input).expect("Issue reading input");
+    
+    if user_input.trim().eq("quit") {
+        game_state.game_over = true;
+    }
+    
+    let parsed_input: Vec<&str> = user_input.trim().split('-').collect();
+    
+    let col_input = convert_character_to_integer(parsed_input[0]);
+    let row_input = parsed_input[1].parse::<usize>().unwrap();
+    game_state.user_input = vec![col_input, row_input];
+
+    game_state
+}
+
+fn convert_character_to_integer(char: &str) -> usize{
+    
+    let mut result: usize = 0;
+
+    if char.eq("a") {
+        result = 0;
+    }
+    else if char.eq("b"){
+        result = 1;
+    }
+    else if char.eq("c"){
+        result = 2;
     }
 
-    game_board[j][col_input] = 1;
-
-    game_board
+    result
 }
 
 fn display_game_board(game_board: [[u32; 3]; 3]){
@@ -48,28 +120,17 @@ fn init_game_board() -> [[u32; 3]; 3]{
 
 fn main() {
 
-    use std::io::{stdin};
+    let mut game_state = GameState{
+        game_over: false,
+        game_board: init_game_board(),
+        user_input: Vec::new()
+    };    
 
-    let mut game_board = init_game_board();
-    
+    while game_state.game_over != true{
 
-    let mut game_over = false;
-    while game_over != true{
-
-        display_game_board(game_board);
-
-        let mut user_input = String::new();
-        stdin().read_line(&mut user_input).expect("Issue reading input");
-        
-        if user_input.trim().eq("quit") {
-            game_over = true;
-        }
-
-        let parsed_input: Vec<&str> = user_input.trim().split('-').collect();
-
-        let col_input = parsed_input[0];
-        let row_input = parsed_input[1].parse::<usize>().unwrap();
-        game_board = mark_spot_on_game_board(game_board, col_input, row_input);
+        display_game_board(game_state.game_board);
+        game_state = mark_spot_for_user(game_state);
+        game_state = mark_spot_for_ai(game_state);
     }
 
     
